51e1530939c8d709cca8f80a3ad3857e03f05c20
israt <israt9528@gmail.com>
2025-10-08 02:35:06 +0600

    added navbar and banner

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import heroImg from '../../assets/hero.png'
+import '../Header/Navbar/Navbar.css'
+import { IoLogoGooglePlaystore } from "react-icons/io5";
+import { FaAppStoreIos } from "react-icons/fa";
+
+
+const Banner = () => {
+    return (
+        <div className='bg-[#f9f9f9] flex flex-col items-center text-center pt-20 px-10'>
+            <h1 className='text-7xl font-medium mb-4 max-md:text-5xl'>We Build <br /> <span className='logo font-bold'>Productive</span> Apps</h1>
+            <p className='text-[#627382] max-md:text-sm'>At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br />Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+            <div className='space-x-4 mt-10'>
+                <button className="btn">
+                    <IoLogoGooglePlaystore />
+                  Google Play
+                </button>
+                <button className="btn">
+                    <FaAppStoreIos />
+                  App Store
+                </button>
+                <div className='mt-10'>
+                    <img src={heroImg} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router';
+import logoImg from '../../../assets/logo.png'
+import './Navbar.css'
+import { ImGithub } from "react-icons/im";
+
+
+
+
+const Navbar = () => {
+    const Links = <>
+    <div className='lg:flex gap-8 font-medium'>
+    <NavLink to='/'><li>Home</li></NavLink>
+    <NavLink><li>Apps</li></NavLink>
+    <NavLink><li>Installation</li></NavLink>
+    </div>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm lg:px-10">
+    <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        {Links}
+      </ul>
+    </div>
+    <div className='flex items-center gap-2'>
+        <img className='w-[40px]' src={logoImg} alt="" />
+    <a className="text-2xl font-bold logo">HERO.IO</a>
+    </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      {Links}
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn navBtn text-white">
+        <ImGithub />
+        Contribute</a>
+  </div>
+  
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
